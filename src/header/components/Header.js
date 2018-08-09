676718106a741ba22284c65282fe059bf4b9a5a1
Luis Escobar <luis.latam@gmail.com>
2018-08-09 14:07:50 -0500

    Header progress 2

--- src/header/components/Header.js
+++ src/header/components/Header.js
@@ -1,28 +1,36 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styled from 'styled-components';
 import Logo from '../images/pf-logo.png'
 
 const Container = styled.div`
   background-color: white;
   display: flex;
+  padding: 20px 30px 20px 30px;
 `;
 
 const LogoContainer = styled.div`
-  flex: 1;
+  flex: 2;
 `;
 
 const NavbarContainer = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
-const NavbarLink = styled.div`
-  flex: 1;
-  flex-direction: row;
+const NavbarLink = styled(NavLink)`
+  flex: 1fr;
+  text-align: center;
+  font-family: sans-serif;
+  font-size: 1em;
+  color: gray;
+  text-decoration: none;
+  margin-left: 20px;
 `;
 
 class Header extends React.Component {
-
   render() {
     return(
       <Container>
@@ -30,17 +38,15 @@ class Header extends React.Component {
           <img src={ Logo } />
         </LogoContainer>
         <NavbarContainer>
-
-          <NavbarLink>
-            <Link to="/">Login</Link>
+          <NavbarLink to="/">
+            Login
           </NavbarLink>
-          <NavbarLink>
-            <Link to="/about">Register</Link>
+          <NavbarLink to="/">
+            Register
           </NavbarLink>
-          <NavbarLink>
-            <Link to="/topics">Search for pets</Link>
+          <NavbarLink to="/">
+            Search for Pets
           </NavbarLink>
-
         </NavbarContainer>
       </Container>
     )
